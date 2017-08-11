ac244b61f6ac4575542a7e14d5decc702f197258
KadyrkeshSultan <sultan.kadyrkesh@yahoo.com>
2017-08-11 12:31:38 +0600

    Add files via upload

---
+++ Models/PrimaryTile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadliner.Models
+{
+    public class PrimaryTile
+    {
+        public static string IdealTime = "Нет задач";
+        public static string IdealMessage = "...или они еще не загружены.";
+        public string time { get; set; } = IdealTime;
+        public string message { get; set; } = IdealMessage;
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadliner.Models
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public bool Complete { get; set; }
+        public string Title { get; set; }
+        public DateTime DueTo { get; set; }
+        public String UserName { get; set; }
+    }
+}
